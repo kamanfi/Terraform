ab6b821b6fdfd1f7669d07f13adeb92dab480b86
Kwasi <kamanfi@gmail.com>
2023-01-20 15:33:31 -0500

    initial

---
+++ ec2/prod/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  default     = "Provisioned by Terraform"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "access_key"
+  default     = "access_key"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "secret_key"
+  default     = "secret_key"
+}
+
