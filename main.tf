003d4a165ed9883d23e4e57b65776ea52d50040d
Kwasi <kamanfi@gmail.com>
2023-01-21 18:04:27 -0500

    add gitignore

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
- terraform {
-   required_providers {
-     aws = {
-       source  = "hashicorp/aws"
-       version = "~> 4.16"
-     }
-   }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 
-   required_version = ">= 1.2.0"
- }
+  required_version = ">= 1.2.0"
+}
 
- provider "aws" {
-   region  = "us-west-2"
- }
+provider "aws" {
+  region = "us-west-2"
+}
 
- resource "aws_instance" "app_server" {
-   ami           = "ami-830c94e3"
-   instance_type = "t2.micro"
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
 
-   tags = {
-     Name = "ExampleAppServerInstance"
-   }
- }
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
